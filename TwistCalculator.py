c01682c93384e60537b330dac5f9ee87133224f4
lindsey-h-andrade <lindsey.andrade@markforged.com>
2017-07-11 18:03:49 -0400

    Cleaned up code and documented some functions and math.

--- TwistCalculator.py
+++ TwistCalculator.py
@@ -90,35 +90,39 @@ def averageSideData(datalist, sample):
 	greenRunsNum = len(greenRawSides) # Number of runs done per sample. Will usually be 3
 	#TODO: Test if samples have more/less than 3 runs. I'm pretty sure it'll work but who knows...
 
+	# Add all the X, Y, and Z values for a given side. (Add all the X's for side 1. Add all the X's for side 2....)
 	for i in range(greenRunsNum): 
 		side1Sum = [x + y for x, y in zip(side1Sum, greenRawSides[i][0])]
 		side2Sum = [x + y for x, y in zip(side2Sum, greenRawSides[i][1])]
 		side3Sum = [x + y for x, y in zip(side3Sum, greenRawSides[i][2])]
 		side4Sum = [x + y for x, y in zip(side4Sum, greenRawSides[i][3])]
 
-
+	# Divide each sum by the number of runs (taking an average)
 	side1Avg = [x / greenRunsNum for x in side1Sum] 
 	side2Avg = [x / greenRunsNum for x in side2Sum]
 	side3Avg = [x / greenRunsNum for x in side3Sum]
 	side4Avg = [x / greenRunsNum for x in side4Sum]
 
+	#Stick it all into an array! 
 	avgData = [side1Avg, side2Avg, side3Avg, side4Avg]
 
 	return avgData
 
 def findAngleBetweenSides(greenAvgData, sinteredAvgData):
+	# Just uses vector math to find the angle between the two vectors. Source for math: http://onlinemschool.com/math/library/vector/angl/ 
 	angle1 = acos(dot(greenAvgData[0], sinteredAvgData[0])/(mag(greenAvgData[0])*mag(sinteredAvgData[0])))
 	angle2 = acos(dot(greenAvgData[1], sinteredAvgData[1])/(mag(greenAvgData[1])*mag(sinteredAvgData[1])))
-	# inside3 = dot(greenAvgData[2], sinteredAvgData[2])/(mag(greenAvgData[2])*mag(sinteredAvgData[2]))
-	# print(inside3)
 	angle3 = acos(dot(greenAvgData[2], sinteredAvgData[2])/(mag(greenAvgData[2])*mag(sinteredAvgData[2])))
 	angle4 = acos(dot(greenAvgData[3], sinteredAvgData[3])/(mag(greenAvgData[3])*mag(sinteredAvgData[3])))
 
 	return [angle1, angle2, angle3, angle4]
+
 def dot(vect1, vect2): 
+	# Just a function to find the dot product between two vectors
 	return vect1[0] * vect2[0] + vect1[1] * vect2[1] + vect1[2] * vect2[2]
 
 def mag(vect): 
+	# Just a function to find magnitudes of vectors
 	return sqrt(vect[0]**2 + vect[1]**2 + vect[2]**2)
 
 
